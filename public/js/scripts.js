d371ab072cf77c5df6bd01464d9db16884acda5f
Ajay Verma <ajay_verma@ajay.av-Compaq-Presario-C700-Notebook-PC>
2016-10-10 19:15:10 +0530

    smoothscroll fixed.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -150,7 +150,7 @@
         $('li a[href*=#]').bind("click", function(e){
             var anchor = $(this);
             $('html, body').stop().animate({
-                scrollTop: $(anchor.attr('href')).offset()
+                scrollTop: $(anchor.attr('href')).offset().top
             }, 1000);
             e.preventDefault();
         });
